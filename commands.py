3b2f24da83ba8673ddfc59accbc304a5fb02f360
Jan <jan.chyb.99@gmail.com>
2021-05-17 12:12:22 +0200

    added worksheet import support, suggestion finder toggle and a new axample xlsx file

--- commands.py
+++ commands.py
@@ -28,10 +28,47 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-def import_data_from_excel(file_name: str) -> pd.DataFrame:
-    df = pd.read_excel(file_name, engine='openpyxl')
-    df = df.where(pd.notnull(df), None)
-    return fill_empty_cells(df)
+def import_data_from_excel(file_name: str) -> list:
+    df_dict = pd.read_excel(file_name, engine='openpyxl', sheet_name=None)
+    df_list = select_sheet(df_dict, file_name)
+    df_cleaned = [fill_empty_cells(df.where(pd.notnull(df), None)) for df in df_list]
+    return df_cleaned
+
+
+def select_sheet(dataframe_dict: dict, file_name: str):
+    keys = list(dataframe_dict.keys())
+    if len(keys) == 1:
+        return [dataframe_dict[keys[0]]]
+    print(f"Multiple worksheets found in {file_name}. List all worksheets you want to import by their number.\n"
+          f"Type \"all\" to select all, or \"end\" to end selection.\n"
+          f"Found worksheets:")
+    for idx, key in enumerate(keys):
+        print(f"({idx}) {key}")
+
+    added = set()
+    while True:
+        print("worksheet_selection>", end='')
+        read = input()
+        if read == "all":
+            added.update(keys)
+            break
+        elif read == "end":
+            break
+        else:
+            try:
+                if int(read) < len(keys) or int(read) > -1:
+                    added.add(keys[int(read)])
+                else:
+                    print(f"{read} - incorrect value.")
+            except ValueError:
+                print(f"{read} - incorrect value.")
+        print(f"Added: {list(added)}")
+
+    res = []
+    for i in added:
+        res += [dataframe_dict[i]]
+    return res
+
 
 
 def fill_empty_cells(df: pd.DataFrame) -> pd.DataFrame:
@@ -76,9 +113,9 @@ def remove_command(excel_list, path: str) -> List[str]:
     return to_remove
 
 
-def check_command(excel_list: List[str], db_mode: bool) -> List[Event]:
+def check_command(excel_list: List[str], db_mode: bool, suggest: bool) -> List[Event]:
+    data_frames = [(excel, data) for excel in excel_list for data in import_data_from_excel(excel)]
     print(bcolors.WARNING, end='')
-    data_frames = [(excel, import_data_from_excel(excel)) for excel in excel_list]
     data_frames = [frame for frame in data_frames if verify_dataframe(frame[1], frame[0])]
 
     events = [Event.create_from_data_frame(table, idx + 1, row) for (table, df) in data_frames
@@ -99,26 +136,25 @@ def check_command(excel_list: List[str], db_mode: bool) -> List[Event]:
             print(elem.event_2)
             print()
         print(bcolors.ENDC, end='')
-
-        events_conflicting = [c.event_1 for c in conflicts] + [c.event_2 for c in conflicts]
-        events_non_conflicting = [e for e in events if e not in events_conflicting]
-        suggestions = find_all_suggestions(conflicts, events_non_conflicting)
-        if suggestions:
-            print(bcolors.OKBLUE, end='')
-            print("Suggestions:\n")
-            for suggestion in suggestions:
-                print("Event:")
-                print_event_label()
-                print(suggestion.conflicting_event)
-                print("Could have its timeframe changed to:")
-                for timeframe in suggestion.timeframes:
-                    print(str(timeframe[0].hour).zfill(2) + ":" + str(timeframe[0].minute).zfill(2) + " - " +
-                          str(timeframe[1].hour).zfill(2) + ":" + str(timeframe[1].minute).zfill(2))
-                print()
-            print(bcolors.ENDC, end='')
+        if suggest:
+            events_conflicting = [c.event_1 for c in conflicts] + [c.event_2 for c in conflicts]
+            events_non_conflicting = [e for e in events if e not in events_conflicting]
+            suggestions = find_all_suggestions(conflicts, events_non_conflicting)
+            if suggestions:
+                print(bcolors.OKBLUE, end='')
+                print("Suggestions:\n")
+                for suggestion in suggestions:
+                    print("Event:")
+                    print_event_label()
+                    print(suggestion.conflicting_event)
+                    print("Could have its timeframe changed to:")
+                    for timeframe in suggestion.timeframes:
+                        print(str(timeframe[0].hour).zfill(2) + ":" + str(timeframe[0].minute).zfill(2) + " - " +
+                              str(timeframe[1].hour).zfill(2) + ":" + str(timeframe[1].minute).zfill(2))
+                    print()
+                print(bcolors.ENDC, end='')
     else:
         return events
-
 
 def print_events_command(events_list: List[Event]):
     if len(events_list) != 0:
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ REMOVE = "remove"
 CHECK = "check"
 PRINT = "print"
 SUBMIT = "submit"
-CHANGE_MODE = "change_mode"
+CHANGE = "change"
 YES = "yes"
 
 list_of_commands = """
@@ -22,7 +22,7 @@ list_of_commands = """
     check - check all newly added files for conflicts
     print <events/files> - print all events that were checked for conflicts and are ready to be submitted or all added files
     submit - send data from added xlsx files if conflicts don't occur 
-    change_mode - change on/off checking with db mode
+    change <mode/suggest> - change on/off checking with database mode <mode> or finding suggestions <suggest>
 """
 
 
@@ -34,11 +34,16 @@ def check_result(result):
 
 
 is_dbmode_on = False
+is_suggest_on = False
 is_yes_awaiting = False
 
 
 def dbmode_str():
     return 'on' if is_dbmode_on else 'off'
+
+
+def suggest_str():
+    return 'on' if is_suggest_on else 'off'
 
 
 def span(predicate, list: List[str]) -> (List[str], List[str]):
@@ -60,69 +65,73 @@ if __name__ == '__main__':
     print("Input commands. Type \"help\" to see a list of possible commands.")
 
     while text != QUIT:
-        print(f"db_mode={dbmode_str()}>", end="")
+        print(f"db_mode={dbmode_str()}|suggest={suggest_str()}>", end="")
         text = input()
         text = text.split(" ")
         command = text[0].lower()
         arg = text[1] if len(text) > 1 else None
-        try:
-            if command == QUIT:
-                if excel_list:
-                    print("Are you sure, that you don't want to check these files", " ".join(excel_list))
-                    print("Write yes if you want to exit without checking files")
-                    text = "are you sure"
-                    is_yes_awaiting = True
-                elif events_list:
-                    print(f"Are you sure, that you don't want to submit {len(events_list)} events to system")
-                    print("Write yes if you want to exit without submitting files")
-                    text = "are you sure"
-                    is_yes_awaiting = True
-                else:
-                    text = QUIT
-            elif command == HELP:
-                print(list_of_commands)
-            elif command == ADD and arg is not None:
-                prev_length = len(excel_list)
-                path = os.path.realpath(arg)
-                add_command_result = check_result(add_command(path))
-                repeated, new_values = span(lambda elem: elem in excel_list, add_command_result)
-                excel_list.extend(new_values)
-                [print(f"File {elem} added again") for elem in repeated]
-                print(f"Successfully added {len(new_values)} excels")
-            elif command == REMOVE and arg is not None:
-                path = os.path.realpath(arg)
-                to_remove = remove_command(excel_list, path)
+        #try:
+        if command == QUIT:
+            if excel_list:
+                print("Are you sure, that you don't want to check these files", " ".join(excel_list))
+                print("Write yes if you want to exit without checking files")
+                text = "are you sure"
+                is_yes_awaiting = True
+            elif events_list:
+                print(f"Are you sure, that you don't want to submit {len(events_list)} events to system")
+                print("Write yes if you want to exit without submitting files")
+                text = "are you sure"
+                is_yes_awaiting = True
+            else:
+                text = QUIT
+        elif command == HELP:
+            print(list_of_commands)
+        elif command == ADD and arg is not None:
+            prev_length = len(excel_list)
+            path = os.path.realpath(arg)
+            add_command_result = check_result(add_command(path))
+            repeated, new_values = span(lambda elem: elem in excel_list, add_command_result)
+            excel_list.extend(new_values)
+            [print(f"File {elem} added again") for elem in repeated]
+            print(f"Successfully added {len(new_values)} excel files")
+        elif command == REMOVE and arg is not None:
+            path = os.path.realpath(arg)
+            to_remove = remove_command(excel_list, path)
+            for i in to_remove:
+                excel_list.remove(i)
+            print(f"Successfully removed {to_remove}.")
+        elif command == CHECK:
+            to_remove = [excel for excel in excel_list if not os.path.isfile(excel)]
+            if len(to_remove) > 0:
                 for i in to_remove:
                     excel_list.remove(i)
-                print(f"Successfully removed {to_remove}.")
-            elif command == CHECK:
-                to_remove = [excel for excel in excel_list if not os.path.isfile(excel)]
-                if len(to_remove) > 0:
-                    for i in to_remove:
-                        excel_list.remove(i)
-                    print(f"Files {to_remove} no longer exist. Removing from an internal list.")
-                events_list = check_result(check_command(excel_list, is_dbmode_on))
-                print(f"Successfully added {len(events_list)} event list(s) without conflicts")
-            elif command == PRINT and arg == "events":
-                print_events_command(events_list)
-            elif command == PRINT and arg == "files":
-                print_files_command(excel_list)
-            elif command == SUBMIT:
-                submit_command(events_list)
-                print(f"{len(events_list)} events were submitted")
-                events_list = []
-                excel_list = []
-            elif command == YES and is_yes_awaiting:
-                text = QUIT
-            elif command == CHANGE_MODE:
+                print(f"Files {to_remove} no longer exist. Removing from an internal list.")
+            events_list = check_result(check_command(excel_list, is_dbmode_on, is_suggest_on))
+            print(f"Successfully added {len(events_list)} events without conflicts")
+        elif command == PRINT and arg == "events":
+            print_events_command(events_list)
+        elif command == PRINT and arg == "files":
+            print_files_command(excel_list)
+        elif command == SUBMIT:
+            submit_command(events_list)
+            print(f"{len(events_list)} events were submitted")
+            events_list = []
+            excel_list = []
+        elif command == YES and is_yes_awaiting:
+            text = QUIT
+        elif command == CHANGE:
+            if arg == 'mode':
                 is_dbmode_on = not is_dbmode_on
                 print(f"DB mode change to {dbmode_str()}")
-            else:
-                print(f"Cannot recognize command {command}. To get all possible commands write help")
+            elif arg == 'suggest':
+                is_suggest_on = not is_suggest_on
+                print(f"Suggestions changed to {suggest_str()}")
+        else:
+            print(f"Cannot recognize command {command}. To get all possible commands write help")
 
-            if command != QUIT:
-                is_yes_awaiting = False
-        except:
-            print(f"Unexpected error occurred. Operation {command} cancelled.")
+        if command != QUIT:
+            is_yes_awaiting = False
+        #except:
+        #    print(f"Unexpected error occurred. Operation {command} cancelled.")
 
     print("Application is closing")
